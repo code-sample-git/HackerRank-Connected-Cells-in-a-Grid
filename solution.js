325d5c66ae1ad7af55ea21525f70915021cdfa3e
Michael Ng <michaelngbusiness@gmail.com>
2022-05-12 13:44:34 +0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -29,10 +29,110 @@ function readLine() {
  * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
  */
 
-function connectedCell(matrix) {
-    // Write your code here
-
+var markRegionArr = []; 
+function connectedCell(matrix, n, m) {
+    // create an array to store the region num
+    for(var a=0; a<n; a++){
+        var row=[];
+        for(var b=0;b<m; b++){
+            //default is 0
+            row.push(0);
+        }
+        markRegionArr.push(row);
+    }
+    
+    //region num starts from 1
+    var newMarker = 1;
+    for(var i=0; i<n; i++){
+        for(var j=0; j<m;j++){
+           if(matrix[i][j] == 1 && markRegionArr[i][j] == 0){
+               //Call DP function to look up all connected cells 
+               markRegion(matrix, i, j, newMarker);
+               
+               //refresh the region num for another region
+               newMarker++;
+           }
+        }
+    }
+    
+    //debug
+    for(var d=0; d< markRegionArr.length; d++){
+        var row = markRegionArr[d];
+        console.log(row.toString());
+    }
+    
+    //Find the large region
+    var largeRegion=0;
+    //region num starts from 1. 0 is not counted as region.
+    for(var nM=1; nM<newMarker; nM++){
+        var count = 0;
+        for(var x=0; x<markRegionArr.length; x++){
+            for(var y=0; y<markRegionArr[x].length; y++){
+                if(markRegionArr[x][y] == nM){
+                    count++;
+                }
+            }
+        }
+        largeRegion = largeRegion < count ? count : largeRegion
+    }
+    
+    return largeRegion;
 }
+
+function markRegion(matrix, i, j, marker){
+     
+    markRegionArr[i][j]=marker;
+    
+    var m = matrix[0].length;
+    var n = matrix.length;
+    
+    if(i>0 && j>0){
+        if(markRegionArr[i-1][j-1] == 0 && matrix[i-1][j-1] ==1){
+            markRegion(matrix, i-1, j-1, marker)
+        }
+    }
+    if(i>0){
+        if(markRegionArr[i-1][j] == 0 && matrix[i-1][j] ==1){
+            markRegion(matrix, i-1, j, marker)
+        }
+    }
+    if(j>0){
+        if(markRegionArr[i][j-1] == 0 && matrix[i][j-1] ==1){
+            markRegion(matrix, i, j-1, marker)
+        }
+    }
+    if(i<n-1){
+        if(markRegionArr[i+1][j] == 0 && matrix[i+1][j] ==1){
+            markRegion(matrix, i+1, j, marker)
+        }
+    }
+    if(j<m-1){
+        //extra logging
+        if(i==1 && j==3){
+            console.log("markRegionArr[i][j+1]: " + markRegionArr[i][j+1]);
+        }
+        if(markRegionArr[i][j+1] == 0 && matrix[i][j+1] ==1){
+            markRegion(matrix, i, j+1, marker)
+        }
+    }
+    if(i<n-1 && j<m-1){
+        if(markRegionArr[i+1][j+1] == 0 && matrix[i+1][j+1] ==1){
+            markRegion(matrix, i+1, j+1, marker)
+        }
+    }
+    if(i<n-1 && j>0){
+        if(markRegionArr[i+1][j-1] == 0 && matrix[i+1][j-1] ==1){
+            markRegion(matrix, i+1, j-1, marker)
+        }
+    }
+    if(j<n-1 && i>0){
+        if(markRegionArr[i-1][j+1] == 0 && matrix[i-1][j+1] ==1){
+            markRegion(matrix, i-1, j+1, marker)
+        }
+    }       
+}
+
+
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
@@ -47,7 +147,7 @@ function main() {
         matrix[i] = readLine().replace(/\s+$/g, '').split(' ').map(matrixTemp => parseInt(matrixTemp, 10));
     }
 
-    const result = connectedCell(matrix);
+    const result = connectedCell(matrix, n, m);
 
     ws.write(result + '\n');
 
